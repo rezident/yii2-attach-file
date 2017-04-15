abb8afca89391b6310e0c89f76b6fa62cb7fc6e8
Yuri Nazarenko <mail@rezident.org>
2017-04-15 17:47:52 +0300

    Replaced the index in the migration

--- migrations/m161221_074034_create_attach_file_table.php
+++ migrations/m161221_074034_create_attach_file_table.php
@@ -26,7 +26,7 @@ class m161221_074034_create_attach_file_table extends Migration
             'uploaded_at' => $this->dateTime()->notNull()
         ]);
 
-        $this->createIndex('idx_model', AttachedFile::tableName(), ['model_key', 'model_id']);
+        $this->createIndex('idx_model_is_main', AttachedFile::tableName(), ['model_key', 'model_id', 'is_main']);
         $this->createIndex('idx_md5_hash', AttachedFile::tableName(), 'md5_hash');
         $this->createIndex('idx_position', AttachedFile::tableName(), 'position');
     }
